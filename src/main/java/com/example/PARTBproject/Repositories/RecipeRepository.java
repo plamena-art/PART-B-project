fb127771a714c25cea046c6a16e1d4ac6bb2bf96
plamena-art <petkova_plamena@icloud.com>
2021-08-19 08:51:05 +1000

    changed dependencies

--- src/main/java/com/example/PARTBproject/Repositories/RecipeRepository.java
+++ src/main/java/com/example/PARTBproject/Repositories/RecipeRepository.java
@@ -3,9 +3,11 @@ package com.example.PARTBproject.Repositories;
 import com.example.PARTBproject.Entity.Recipe;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface RecipeRepository extends CrudRepository<Recipe, Long> {
+    void saveAll(List<Recipe> recipes);
 
     //Optional<Recipe> findById(Long recipeId)
 
